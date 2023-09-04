082f93de7207fbfe0b25d7b5ad84dda082b085a8
Sudoolashraf <142524366+Sudoolashraf@users.noreply.github.com>
2023-09-04 17:34:38 +0300

    Create BubbleSort.js

---
+++ BubbleSort.js
@@ -0,0 +1,71 @@
+const bblSort = (arr) => {
+
+    for (let i = 0; i < arr.length; i++) {
+  
+        // Last i elements are already in place  
+        for (let j = 0; j < (arr.length - i - 1); j++) {
+  
+            // Checking if the item at present iteration 
+            // is greater than the next iteration
+            if (arr[j] > arr[j + 1]) {
+  
+                // If the condition is true
+                // then swap them
+                let temp = arr[j]
+                arr[j] = arr[j + 1]
+                arr[j + 1] = temp
+            }
+        }
+    }
+  
+    // Print the sorted array
+    console.log(arr);
+}
+  
+// This is our unsorted array
+let arr = [20, 40, 50, 300, 7, 1500, 30, 14];
+  
+// Now pass this array to the bblSort() function
+bblSort(arr);
+
+
+
+
+
+// Optimized implementation of bubble sort Algorithm
+  
+const OptmizeBubbleSort = (arr) => {
+
+    let i, j;
+    let len = arr.length;
+  
+    let isSwapped = false;
+  
+    for (i = 0; i < len; i++) {
+  
+        isSwapped = false;
+  
+        for (j = 0; j < len; j++) {
+            if (arr[j] > arr[j + 1]) {
+                let temp = arr[j]
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                isSwapped = true;
+            }
+        }
+  
+        // IF no two elements were swapped
+        // by inner loop, then break 
+        if (!isSwapped) {
+            break;
+        }
+    }
+  
+    // Print the array
+    console.log(arr)
+}
+  
+let array = [20, 40, 50, 300, 7, 1500, 30, 14];
+  
+// calling the bubbleSort Function
+OptmizeBubbleSort(array)
